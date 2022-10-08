3c90886a97db9aa9962ba1fd9073abf6a00bcece
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-09 00:17:57 +0900

    fix: background

---
+++ index.js
@@ -0,0 +1,7 @@
+const root = document.getElementById("root");
+
+const backgroundContainer = document.createElement("div");
+backgroundContainer.classList.add("container-fluid", "bg-info", "text-center", "background");
+backgroundContainer.style.height = "50vh";
+
+root.append(backgroundContainer);
