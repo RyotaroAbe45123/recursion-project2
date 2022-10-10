88f8f9f5761a0d58ce89272730b96ad896e2dc8c
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-10-10 22:00:22 +0900

    update: image src

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
 const images = [
     {
-        name: "mercury",
-        path: "assets/mercury.jpg",
+        name: "Mercury",
+        diameter: "4879.4",
+        path: "https://upload.wikimedia.org/wikipedia/commons/3/30/Mercury_in_color_-_Prockter07_centered.jpg",
     },
     {
-        name: "venus",
-        path: "assets/venus.jpg"
+        name: "Venus",
+        diameter: "12103.6",
+        path: "https://upload.wikimedia.org/wikipedia/commons/1/1f/Galileo_Venus_global_view.jpg"
     },
     {
-        name: "earth",
-        path: "assets/earth.jpg"
+        name: "Earth",
+        diameter: "12756.274",
+        path: "https://upload.wikimedia.org/wikipedia/commons/c/cb/The_Blue_Marble_%28remastered%29.jpg"
     },
     {
-        name: "mars",
-        path: "assets/mars.jpg"
+        name: "Mars",
+        diameter: "6794.4",
+        path: "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg"
     },
     {
-        name: "jupiter",
-        path: "assets/jupiter.jpg"
+        name: "Jupiter",
+        diameter: "142984",
+        path: "https://upload.wikimedia.org/wikipedia/commons/5/5a/Jupiter_by_Cassini-Huygens.jpg"
     },
     {
-        name: "saturn",
-        path: "assets/saturn.jpg"
+        name: "Saturn",
+        diameter: "120536",
+        path: "https://upload.wikimedia.org/wikipedia/commons/2/25/Saturn_PIA06077.jpg"
     },
     {
-        name: "uranus",
-        path: "assets/uranus.jpg"
+        name: "Uranus",
+        diameter: "51118",
+        path: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg"
     },
     {
-        name: "hoge",
-        diameter: "100",
-        path: "https://upload.wikimedia.org/wikipedia/commons/3/30/Mercury_in_color_-_Prockter07_centered.jpg"
+        name: "Neptune",
+        diameter: "49528",
+        path: "https://upload.wikimedia.org/wikipedia/commons/6/63/Neptune_-_Voyager_2_%2829347980845%29_flatten_crop.jpg"
     }
 ];
 images.forEach((image) => {
     image.component = `
-        <img class="col-10 image" src=${image.path} alt=${image.name}  />
+        <img class="image" src=${image.path} alt=${image.name}  />
     `
 });
 
@@ -44,10 +51,11 @@ const initialize = () => {
     
     root.innerHTML = `
         <div class="d-flex vh-100 justify-content-center align-items-center">
-            <div class="d-flex bg-info flex-wrap col-8">
+            <div class="d-flex bg-info flex-wrap col-10">
                 <div id="sliderContainer" class="d-flex col-7 p-2 justify-content-center align-items-center">
                 </div>
                 <div class="col-5 p-2">
+                    <div id="titleContainer" class="text-center text-uppercase text">solar system</div>
                     <div id="infoContainer"></div>
                     <div id="buttonContainer"></div>
                 </div>
@@ -63,7 +71,7 @@ const createButtons = () => {
     numbers.forEach((number) => {
         htmlString += `
             <div class="col-4 text-center p-2">
-                <button class="btn btn-light col-12">${number}</button>
+                <button class="btn btn-light col-12">${images[number-1].name}</button>
             </div>
         `
     })
@@ -81,7 +89,7 @@ const createButtons = () => {
 const createSlider = () => {
     const sliderContainer = document.getElementById("sliderContainer")
     sliderContainer.innerHTML = `
-        <div id="sliderShow" class="d-flex col-12">
+        <div id="sliderShow" class="d-flex col-8">
             <div id="main" class="main full-width" data-index=0></div>
             <div id="extra" class="extra full-width"></div>
         </div>
@@ -98,7 +106,7 @@ const createInfoContainer = (index) => {
     infoContainer.innerHTML = `
         <div>
             <div>Name: ${images[index-1].name}</div>
-            <div>Diameter: ${images[index-1].diameter} m</div>
+            <div>Diameter: ${Number(images[index-1].diameter).toLocaleString("en-IN", {maximumSignificantDigits: 5})} km</div>
         </div>
     `
 }
